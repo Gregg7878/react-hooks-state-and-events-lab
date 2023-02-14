9a84b25489de18ae9420290ec8673ca4b09815b7
Gregg7878 <theheartbreakkidd777@gmail.com>
2023-02-15 01:02:20 +0300

    finished the Item.js

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,9 +1,9 @@
 import React,{ useState} from "react";
 
 function Item({ name, category }) {
-  const [isInCart, setIsInCart]= useState(false);
+  const [isInCart, setIsInCart] = useState(false);
 
-  const toggleCart =() => {
+  const toggleCart = () => {
     setIsInCart((prevValue) => !prevValue);
   };
 
