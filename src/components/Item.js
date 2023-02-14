2088f10042118e37be141ed6f8c8d775645cc1d8
Gregg7878 <theheartbreakkidd777@gmail.com>
2023-02-15 00:55:33 +0300

    finished with App.js finished with Item.js finished with ShoppingList.js

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React,{ useState} from "react";
 
 function Item({ name, category }) {
+  const [isInCart, setIsInCart]= useState(false);
+
+  const toggleCart =() => {
+    setIsInCart((prevValue) => !prevValue);
+  };
+
   return (
-    <li className="">
-      <span>{name}</span>
-      <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+    <li className={isInCart ? "in-cart" : ""}>
+      {name} - ${price}
+      <button onClick={toggleCart}>
+        {isInCart ? "Remove From Cart" : "Add to Cart"}
+      </button>
     </li>
   );
 }
